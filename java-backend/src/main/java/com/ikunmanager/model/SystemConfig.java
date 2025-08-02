87d999fffafedff93e27e741e8a3a17615ba95e6
chyinan <chyinan2015@gmail.com>
2025-08-02 11:19:12 +0800

    重构SystemConfig类，使用Lombok注解自动生成getter和setter方法，简化代码结构，提升可读性。

--- java-backend/src/main/java/com/ikunmanager/model/SystemConfig.java
+++ java-backend/src/main/java/com/ikunmanager/model/SystemConfig.java
@@ -1,15 +1,21 @@
 package com.ikunmanager.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "system_config")
 public class SystemConfig {
 
+    // Getters and Setters
     @Id
     @Column(name = "config_key")
     private String configKey;
@@ -23,36 +29,4 @@ public class SystemConfig {
     @Column(name = "update_time")
     private LocalDateTime updateTime;
 
-    // Getters and Setters
-    public String getConfigKey() {
-        return configKey;
-    }
-
-    public void setConfigKey(String configKey) {
-        this.configKey = configKey;
-    }
-
-    public String getConfigValue() {
-        return configValue;
-    }
-
-    public void setConfigValue(String configValue) {
-        this.configValue = configValue;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
 }
